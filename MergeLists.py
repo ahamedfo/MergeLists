a0fefb4a942159d906f18299f59cd91158046b75
ahamedfofana <ahamedfofana12@gmail.com>
2020-05-04 19:12:14 -0400

    Initial Commit

--- MergeLists.py
+++ MergeLists.py
@@ -1,12 +1,34 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
+        i = 0
+        k = 0
+        header = ListNode(-1)
+        cur = header
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+
+        while l1:
+            cur.next = l1
+            l1
+
+        return header.next
+
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
+
+-1 -> 1 -> 1

fe7b9585c528564e2aa04aab5aa5410be30612a4
ahamedfofana <ahamedfofana12@gmail.com>
2020-05-06 17:32:03 -0400

    Initial Commit

--- MergeLists.py
+++ MergeLists.py
@@ -4,6 +4,7 @@ class ListNode(object):
         self.val = val
         self.next = next
 
+
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
         i = 0
@@ -21,14 +22,18 @@ class Solution(object):
 
         while l1:
             cur.next = l1
-            l1
+            l1 = l1.next
+            cur = cur.next
+
+        while l2:
+            cur.next = l2
+            l2 = l2.next
+            cur = cur.next
 
         return header.next
-
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
 
--1 -> 1 -> 1
